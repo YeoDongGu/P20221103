996f2c0dc547899496894d1e13392c2f4e5d8245
Administrator <Administrator@User-2022JIONJU>
2022-11-04 18:02:20 +0900

    d

---
+++ P20221103/src/While/Main3.java
@@ -0,0 +1,21 @@
+package While;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+
+		while (true) {
+			System.out.println(i);
+
+			if (i == 6) {
+				break;
+			}
+			
+			i = i + 1;
+		}
+
+	}
+
+}
---
+++ P20221103/src/While/Main4.java
@@ -0,0 +1,15 @@
+package While;
+
+public class Main4 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 != 0)
+				continue;
+
+			System.out.println(i);
+		}
+	}
+
+}
